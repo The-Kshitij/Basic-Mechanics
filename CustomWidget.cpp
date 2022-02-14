790d4ba22667a362bbbfafe921bcdad6e6ad0451
The-Kshitij <55736254+The-Kshitij@users.noreply.github.com>
2022-02-14 11:35:55 +0530

    Update CustomWidget.cpp

--- CustomWidget.cpp
+++ CustomWidget.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CustomWidget.h"
 #include "Components/TextBlock.h"
 #include "Components/ProgressBar.h"
@@ -14,9 +11,6 @@ bool UCustomWidget::Initialize()
 		{
 			bSuccess = false;
 		}
-	}
-	if (bSuccess)
-	{
 		UE_LOG(LogTemp,Warning,TEXT("(CustomWidget)Initialization of CustomWidget successfull"));
 	}
 	else
